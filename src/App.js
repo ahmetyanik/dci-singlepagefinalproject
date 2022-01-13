fa3762a7db6a83c3157b620267e4ec0da337e4ef
ahmetyanik <ahmetyank4242@gmail.com>
2022-01-13 16:00:34 +0100

    loginpage

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import "./App.css";
 import Homepage from "./components/Pages/Homepage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Loginpage from "./components/Pages/Loginpage";
-import Register from "./components/Pages/Register";
+import { useEffect, useState } from "react";
+import users from "./datenbank/users";
 
 function App() {
+
+  const [allUsers,setAllUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(()=>{
+
+    setAllUsers(users);
+    
+  },[])
+
+
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Homepage />} />
-          <Route path="/login"  element={<Loginpage/>}/>
-          <Route path="/register"  element={<Register/>}/>
+          <Route path="/login"  element={<Loginpage allUsers={allUsers} setCurrentUser={setCurrentUser}/>}/>
         </Routes>
       </BrowserRouter>
     </div>
