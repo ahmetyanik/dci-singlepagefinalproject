3a252b42aaa52beb97ea552bf303fb3869b98597
ahmetyanik <ahmetyank4242@gmail.com>
2022-01-13 10:10:17 +0100

    Datenbank created

---
+++ src/datenbank/books.js
@@ -0,0 +1,25 @@
+const books = [
+    {
+        title: "The Promise",
+        author : "Damon Galgut",
+        image: "https://www.kulturkaufhaus.de/annotstream/9781784744076/COPL/Galgut-Damon/The-Promise.jpg?sq=5",
+        price :"15.50",
+        language : "Englisch",
+        pages : "304",
+        description : `WINNER OF THE BOOKER PRIZE 2021
+        Discover the powerful prizewinning story of a family in crisis.
+        'Astonishing' Colm Tóibín
+        The Promise charts the crash and burn of a white South African family, living on a farm outside Pretoria. The Swarts are gathering for Ma's funeral. The younger generation, Anton and Amor, detest everything the family stand for - not least the failed promise to the Black woman who has worked for them her whole life. After years of service, Salome was promised her own house, her own land... yet somehow, as each decade passes, that promise remains unfulfilled.
+        The narrator's eye shifts and blinks: moving fluidly between characters, flying into their dreams; deliciously lethal in its observation. And as the country moves from old deep divisions to its new so-called fairer society, the lost promise of more than just one family hovers behind the novel's title.
+        In this story of a diminished family, sharp and tender emotional truths hit home. Confident, deft and quietly powerful, The Promise is literary fiction at its finest.
+        'Gorgeous and pleasurable' Tessa Hadley
+        'The most important book of the last ten years' Edmund White
+        'Simply: you must read it' Claire Messud`,
+        kategorie:"Roman",
+        "ISBN/GTIN": "978-3-630-87707-5",
+        Produktart: "Buch"        
+
+    }
+]
+
+export default books;
