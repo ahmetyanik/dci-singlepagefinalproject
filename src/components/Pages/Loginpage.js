20455b9a7463b97a01b41daffa689990a3cb2439
ahmetyanik <ahmetyank4242@gmail.com>
2022-01-13 15:16:55 +0100

    login

--- src/components/Pages/Loginpage.js
+++ src/components/Pages/Loginpage.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Footer from "../Footer";
 import Header from "../Header";
+import Logintemplate from "../Logintemplate";
 
 function Loginpage() {
   return (
     <div>
       <Header />
+      <div className="my-5">
+      
+      <Logintemplate />
 
-    
-
+      </div>
       <Footer />
     </div>
   );
