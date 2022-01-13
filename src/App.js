97eb470f066edd00b8a22cec3e8c6d511a852685
siddik <spolatde@gmail.com>
2022-01-13 13:30:38 +0100

    footer added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import books from './datenbank/books';
 
@@ -7,6 +8,8 @@ function App() {
     <div className="App">
   
     <Header/>
+
+    <Footer/>
 
     </div>
   );
