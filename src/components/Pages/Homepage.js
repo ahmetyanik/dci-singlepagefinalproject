e8d30f02f25af1404e26cc5694263ab5524f5303
ahmetyanik <ahmetyank4242@gmail.com>
2022-01-13 14:54:39 +0100

    Pages created

---
+++ src/components/Pages/Homepage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header";
+import Carousel from "../Carousel";
+import Footer from "../Footer";
+
+function Homepage() {
+  return (
+    <div>
+      <Header />
+      <Carousel />
+      <Footer />
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/components/Pages/Loginpage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../Footer";
+import Header from "../Header";
+
+function Loginpage() {
+  return (
+    <div>
+      <Header />
+
+    
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Loginpage;
