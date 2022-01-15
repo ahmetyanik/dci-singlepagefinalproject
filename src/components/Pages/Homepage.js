9a7ba4a4f65cec4a4d19bdb85ac2d71012388f0b
ahmetyanik <ahmetyank4242@gmail.com>
2022-01-15 11:23:22 +0100

    userpagearea current person

--- src/components/Pages/Homepage.js
+++ src/components/Pages/Homepage.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../Header";
 import Carousel from "../Carousel";
 import Footer from "../Footer";
+import BookCard from "../BookCard";
 
 function Homepage() {
   return (
     <div>
       <Header />
       <Carousel />
+      <BookCard/>
       <Footer />
     </div>
   );
--- src/components/Userpagearea.js
+++ src/components/Userpagearea.js
@@ -1,15 +1,26 @@
-import React, { useContext } from 'react'
-import DataStore from './DataStore';
+import React, { useContext } from "react";
+import DataStore from "./DataStore";
 
 function Userpagearea() {
+  const { currentUser, setCurrentUser } = useContext(DataStore);
 
-    const { currentUser, setCurrentUser } = useContext(DataStore);
-
-    return (
-        <div>
-            <h1>{currentUser.name}</h1>
+  return (
+    <div>
+      <h1 className="text-dark">{currentUser.name}</h1>
+      <div className="row" style={{ minHeight: "70vh" }}>
+        <div style={{height:"200px"}} className="col-sm-12 col-lg-6 d-lg-flex flex-column flex-lg-row p-lg-3 border shadow">
+          <img className="mx-lg-5" style={{ width: "200px" }} src={currentUser.image} alt="" />
+          <div className="d-flex flex-column align-items-start" >
+        <div>Name: {currentUser.name}</div>
+        <div>User Id: {currentUser.id}</div>
+        <div>E-Mail: {currentUser.email}</div>
+        <div>Credit Card: {currentUser.creditCard}</div>
+        <div>Address: {currentUser.address}</div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Userpagearea
+export default Userpagearea;
