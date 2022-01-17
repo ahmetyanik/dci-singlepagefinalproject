8a11508f19cb7dfec132f6d0d31510c547943661
ahmetyanik <ahmetyank4242@gmail.com>
2022-01-17 13:20:06 +0100

    Adminpage

---
+++ src/components/Pages/Adminpage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "../Header";
+import Footer from "../Footer";
+import Adminform from '../Adminform';
+
+function Adminpage() {
+    return (
+        <div>
+            <Header/>
+            <Adminform/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Adminpage
