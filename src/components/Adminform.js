7542c47d013824d008ddcb0d60bac0aff3bda499
Felix Hasse <f.hasse@gmail.com>
2022-01-17 16:07:25 +0100

    admin form

--- src/components/Adminform.js
+++ src/components/Adminform.js
@@ -2,8 +2,46 @@ import React from 'react'
 
 function Adminform() {
     return (
-        <div>
-            Hier ist admin form
+        <div className='d-flex justify-content-center' >
+             <form class="d-flex flex-column">
+             <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Titel</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Autor</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Image</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Preis</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Sprache</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Seiten</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Beschreibung</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label"> Kategorie</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Produktart</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Einbandart</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Verlag</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Erscheinungsjahr</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Erstverkaufstag</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Gewicht</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Artikelnummer</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" class="form-label">Genre</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
         </div>
     )
 }
